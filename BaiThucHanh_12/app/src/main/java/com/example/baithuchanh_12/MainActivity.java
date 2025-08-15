4f392723b1ed44bca034fa98b676f2ef62ff5bd2
manhduc <2251172281@e.tlu.edu.vn>
2025-08-15 14:24:32 +0700

    Upload bài 1 bài thực hành 12

---
+++ BaiThucHanh_12/app/src/main/java/com/example/baithuchanh_12/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.baithuchanh_12;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    TextView txt1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt1 = findViewById(R.id.selection);
+        final String arr[] = {"iPhone 7", "Samsung Galaxy S7", "Nokia Lumia 730", "Sony Xperia XZ", "HTC One E9"};
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arr);
+
+        ListView lv1 = findViewById(R.id.lv1);
+        lv1.setAdapter(adapter1);
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int i, long arg3) {
+                // TODO Auto-generated method stub
+                txt1.setText("Vị trí " + i + " : " + arr[i]);
+            }
+        });
+    }
+}

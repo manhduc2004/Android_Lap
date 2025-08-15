925deb626154f35daefdb9d88225eb4dde9817b6
manhduc <2251172281@e.tlu.edu.vn>
2025-08-15 16:02:52 +0700

    Upload bài 3 bài thực hành 12

---
+++ BaiThucHanh_12_3/app/src/main/java/com/example/baithuchanh_12_3/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.baithuchanh_12_3;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    String arr1[] = {"Hàng Điện tử", "Hàng Hóa chất", "Hàng Gia dụng", "Hàng xây dựng"};
+
+    TextView txt1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txt1 = (TextView) findViewById(R.id.txt1);
+        Spinner spin1 = (Spinner) findViewById(R.id.spinner1);
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_item, arr1);
+        adapter1.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+        spin1.setAdapter(adapter1);
+
+        spin1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // TODO Auto-generated method stub
+                txt1.setText(arr1[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
+    }
+}
